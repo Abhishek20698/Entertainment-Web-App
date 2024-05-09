6dde2eb166a2294bc90ccc256e186cef2431eb21
Abhishek20698 <abhishekchadar1998@gmail.com>
2024-05-09 10:14:35 +0530

    enterntainment

--- frontend/src/utils/baseUrl.js
+++ frontend/src/utils/baseUrl.js
@@ -1,4 +1,4 @@
 //  const baseUrl = "http://localhost:8000/api";
- const baseUrl = "http://localhost:8000/api/";
+ const baseUrl = "https://entertainment-web-app-avbackend.vercel.app/api";
 
  export default baseUrl;
